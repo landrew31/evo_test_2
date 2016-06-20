bc643b859be72e8877687cb68bf6693ee61d3806
landrew31 <lupa_andriy@list.ru>
2016-06-20 12:27:42 +0300

    basic finish

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import os, requests, json
+import random
 
 
 app = Flask(__name__)
@@ -18,8 +21,23 @@ def index():
 @app.route('/sayHello', methods=['POST'])
 def sayHello():
     name =  request.form['name'];
+    person = db.session.query(Name).filter_by(name=name).first()
+    print person is None
+    if person is None:
+    	adj_count = Adjective.query.count()
+    	new_adj = random.randint(0, adj_count)
+    	while db.session.query(Adjective).filter_by(id=new_adj) is None:
+    		new_adj = random.randint(0, adj_count)
+    	new_person = Name(name, new_adj)
+    	db.session.add(new_person)
+    	db.session.commit()
+    	hello = 'Рад тебя видеть, '.decode('utf-8') + db.session.query(Adjective).filter_by(id=new_person.adj_id).first().adj + " " + new_person.name + "!"
+    else:
+    	hello = "Рад тебя снова видеть, ".decode('utf-8') + db.session.query(Adjective).filter_by(id=person.adj_id).first().adj + " " + person.name + "!"
 
-    return json.dumps({'status':'OK','hello':name});
+
+
+    return json.dumps({'status':'OK','hello': hello});
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -7,7 +7,10 @@ class Adjective(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     adj = db.Column(db.String(50))
-    name = db.relationship('Name', backref='adjective', lazy='dynamic', uselist=False)
+    name = db.relationship('Name', backref='adjective', uselist=False)
+
+    def  __init__(self, adj):
+        self.adj = adj
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
@@ -18,6 +21,10 @@ class Name(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     adj_id = db.Column(db.Integer, db.ForeignKey('adjective.id'))
+
+    def __init__(self,name,adj_id):
+        self.name = name
+        self.adj_id = adj_id
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
