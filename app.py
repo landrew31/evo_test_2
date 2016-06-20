81b68633dcb1a6329a8ea2b93f8be21002dc4836
landrew31 <lupa_andriy@list.ru>
2016-06-20 10:27:16 +0300

    basic hello

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-import os
+import os, requests, json
 
 
 app = Flask(__name__)
@@ -12,14 +12,15 @@ from models import Adjective, Name
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route('/sayHello', methods=['POST'])
+def sayHello():
+    name =  request.form['name'];
+    print name
+    return json.dumps({'status':'OK','name':name});
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True, debug=True)
